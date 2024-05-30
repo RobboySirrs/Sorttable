d16c54904fbbddf72e0bca78723fd0a9bd61c6ef
RobboySirrs <97877286+RobboySirrs@users.noreply.github.com>
2024-05-30 23:45:10 +0200

    Add files via upload
    Rewritten code new start

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const table = document.getElementById("theTable");
 const thsort = table.querySelectorAll("th");
-let trs = table.querySelectorAll("tr:not(:first-child)");
-let params = new URLSearchParams(document.location.search);
-let currentSort = params.get("sort");
 
 //ORDNING
 
@@ -10,17 +7,10 @@ let currentSort = params.get("sort");
 ///lägg till kommentrarer
 //Testar lejer
 
-var element = document.createElement("i");
+/* var element = document.createElement("i");
 
 for (var i = 0; i < thsort.length; i++) {
-  if (!params.get("sort")) {
-    break;
-  }
-  let currentSort2 = params.get("sort").split("-");
-  if (thsort[i].textContent != currentSort2[0]) {
-    continue;
-  }
-
+  
   if (thsort[i].dataset.type === "number") {
     if (currentSort2[1] === "DESC") {
       element.classList.add("down", "arrow");
@@ -56,6 +46,8 @@ for (var i = 0; i < thsort.length; i++) {
   } else {
     element.classList.add("up", "arrow");
     thsort[i].appendChild(element);
+
+
     Array.from(trs)
       .sort((a, b) =>
         a.cells[i].textContent.toLowerCase() >
@@ -65,7 +57,70 @@ for (var i = 0; i < thsort.length; i++) {
       )
       .forEach((tr) => table.appendChild(tr));
     break;
+
   }
+} */
+const acsSort = (element, dataSort) => {
+
+  let trs = table.querySelectorAll("tr:not(:first-child)");
+  let elements = Array.from(element.parentNode.children)
+  elements.forEach((x) => x.removeAttribute("data-sort"));
+  //Index of the table Row.
+  let index = elements.indexOf(element)
+  element.setAttribute("data-sort", "ACS");
+  //check if if it so be sortet as number or text or date???
+  if(element.dataset.type === "number") {
+      Array.from(trs)
+        .sort((a, b) => b.cells[index].textContent - a.cells[index].textContent)
+        .forEach((tr) => table.appendChild(tr));
+      } else {
+        Array.from(trs)
+        .sort((a, b) =>
+          a.cells[index].textContent.toLowerCase() >
+          b.cells[index].textContent.toLowerCase()
+            ? 1
+            : -1
+        )        .forEach((tr) => table.appendChild(tr));
+
+      }
+      
+
+}
+
+const decsSort = (element, dataSort) => {
+  let trs = table.querySelectorAll("tr:not(:first-child)");
+  let elements = Array.from(element.parentNode.children)
+  elements.forEach((x) => x.removeAttribute("data-sort"));
+  //Index of the table Row.
+  let index = Array.from(element.parentNode.children).indexOf(element)
+  element.setAttribute("data-sort", "DECS");
+
+  //check if if it so be sortet as number or text or date???
+  if(element.dataset.type === "number") {
+      Array.from(trs)
+        .sort((a, b) => a.cells[index].textContent - b.cells[index].textContent)
+        .forEach((tr) => table.appendChild(tr));
+      } else {
+        Array.from(trs)
+        .sort((a, b) =>
+          a.cells[index].textContent.toLowerCase() <
+          b.cells[index].textContent.toLowerCase()
+            ? 1
+            : -1
+        )        .forEach((tr) => table.appendChild(tr));
+
+      }
+  
+}
+
+
+const sort = (element, dataSort) => {
+  
+  if (dataSort == "ACS") {
+    decsSort(element, dataSort); 
+    return;
+  }
+  acsSort(element, dataSort); 
 }
 
 //EVENTLISTENER
@@ -73,13 +128,7 @@ const th = table.querySelectorAll("th:not(.nosort)");
 
 th.forEach((element) => {
   element.addEventListener("click", function () {
-    const order = element.textContent;
-    if (currentSort == order + "-ASC") {
-      params.set("sort", order + "-DESC");
-      window.location.search = params.toString();
-    } else {
-      params.set("sort", order + "-ASC");
-      window.location.search = params.toString();
-    }
+   let dataSort = element.getAttribute("data-sort")
+    sort(element, dataSort) 
   });
 });
